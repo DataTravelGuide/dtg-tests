d9d1b17b978125f5a88ee86715d097ff69a0946c
Dongsheng Yang <dongsheng.yang@linux.dev>
2025-07-22 10:37:03 +0800

    Add misc test for cache_mode change failure

---
+++ pcache.py.data/pcache_misc_tests/case15_fail_after_cache_mode_change.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+: "${cache_mode:=writeback}"
+reset_pmem
+
+echo "DEBUG: case 15 - dmsetup create should fail after cache_mode change"
+SEC_NR=$(sudo blockdev --getsz ${data_dev0})
+sudo dmsetup create ${dm_name0} --table "0 ${SEC_NR} pcache ${cache_dev0} ${data_dev0} 4 cache_mode ${cache_mode} data_crc ${data_crc}"
+sudo dmsetup remove ${dm_name0}
+
+if [[ "${cache_mode}" == "writeback" ]]; then
+    new_mode=writethrough
+else
+    new_mode=writeback
+fi
+if sudo dmsetup create ${dm_name0} --table "0 ${SEC_NR} pcache ${cache_dev0} ${data_dev0} 4 cache_mode ${new_mode} data_crc ${data_crc}"; then
+    echo "dmsetup create succeeded after cache_mode change"
+    sudo dmsetup remove ${dm_name0}
+    exit 1
+fi
+
+sudo rmmod dm-pcache 2>/dev/null || true
