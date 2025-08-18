36755e937b71c84fe53f0b36ba9babfca9f90075
Dongsheng Yang <dongsheng.yang@linux.dev>
2025-08-18 13:43:48 +0000

    add smatch for buid
    Signed-off-by: Dongsheng Yang <dongsheng.yang@linux.dev>

--- pcache.py.data/pcache_build.sh
+++ pcache.py.data/pcache_build.sh
@@ -9,6 +9,9 @@ cleanup() {
 }
 trap cleanup EXIT
 
+make clean M=drivers/md/dm-pcache
 make C=1 M=drivers/md/dm-pcache
+make clean M=drivers/md/dm-pcache
+make M=drivers/md/dm-pcache/  C=2 CHECK="/workspace/smatch/smatch -p=kernel --full-path" -j 32
 make htmldocs SPHINXDIRS=admin-guide/device-mapper SPHINXOPTS="-W -n -j1 --keep-going -D suppress_warnings=ref.doc"
 
