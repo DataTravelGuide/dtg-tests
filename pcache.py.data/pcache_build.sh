c92e4d4f4d74397d8bb5b16b34433eca06124517
Dongsheng Yang <dongsheng.yang@linux.dev>
2025-08-18 21:59:23 +0800

    update build
    Signed-off-by: Dongsheng Yang <dongsheng.yang@linux.dev>

--- pcache.py.data/pcache_build.sh
+++ pcache.py.data/pcache_build.sh
@@ -12,6 +12,7 @@ trap cleanup EXIT
 make clean M=drivers/md/dm-pcache
 make C=1 M=drivers/md/dm-pcache
 make clean M=drivers/md/dm-pcache
-make M=drivers/md/dm-pcache/  C=2 CHECK="/workspace/smatch/smatch -p=kernel --full-path" -j 32
+make M=drivers/md/dm-pcache/  C=2 CHECK="/workspace/smatch/smatch -p=kernel --full-path" -j 32 2>&1 | tee smatch.out
+grep -Ei 'warn|error' smatch.out && exit 1
 make htmldocs SPHINXDIRS=admin-guide/device-mapper SPHINXOPTS="-W -n -j1 --keep-going -D suppress_warnings=ref.doc"
 
