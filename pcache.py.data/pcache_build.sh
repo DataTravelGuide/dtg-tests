a6f454b36c9115e957d326a5c62c58e2f33853d9
Dongsheng Yang <dongsheng.yang@linux.dev>
2025-08-22 10:05:43 +0800

    Add optional 32-bit pcache build test

--- pcache.py.data/pcache_build.sh
+++ pcache.py.data/pcache_build.sh
@@ -2,6 +2,7 @@
 set -ex
 
 : "${linux_path:=/workspace/linux_compile}"
+: "${kernel_dir_32:=/workspace_data/linux_compile_32/}"
 
 pushd "$linux_path"
 cleanup() {
@@ -15,4 +16,13 @@ make clean M=drivers/md/dm-pcache
 make M=drivers/md/dm-pcache/  C=2 CHECK="/workspace/smatch/smatch -p=kernel --full-path" -j 32 2>&1 | tee smatch.out
 grep -Ei 'warn|error' smatch.out && exit 1
 make htmldocs SPHINXDIRS=admin-guide/device-mapper SPHINXOPTS="-W -n -j1 --keep-going -D suppress_warnings=ref.doc"
+
+if [ -d "$kernel_dir_32" ]; then
+    pushd "$kernel_dir_32"
+    make clean M=drivers/md/dm-pcache/
+    make -j 64 ARCH=i386 M=drivers/md/dm-pcache/
+    popd
+else
+    echo "kernel_dir_32 not found ($kernel_dir_32), skipping 32-bit build."
+fi
 
