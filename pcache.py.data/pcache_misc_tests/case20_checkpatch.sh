8eb6ef18ff3b2b032675e110d8a8b178d7d2e16b
Dongsheng Yang <dongsheng.yang@linux.dev>
2025-09-03 09:10:38 +0800

    stash in checkpatch.pl
    Signed-off-by: Dongsheng Yang <dongsheng.yang@linux.dev>

--- pcache.py.data/pcache_misc_tests/case20_checkpatch.sh
+++ pcache.py.data/pcache_misc_tests/case20_checkpatch.sh
@@ -5,6 +5,13 @@ set -ex
 
 cd "${linux_path}"
 
+need_pop=0
+if [ -n "$(git status --porcelain)" ]; then
+    git stash -q
+    need_pop=1
+fi
+trap '[ "$need_pop" -eq 1 ] && git stash pop -q' EXIT
+
 for file in $(git ls-files drivers/md/dm-pcache/); do
     ./scripts/checkpatch.pl --fix-inplace "$file"
     if [[ $? -ne 0 ]]; then
